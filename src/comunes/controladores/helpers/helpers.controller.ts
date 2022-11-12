79cdffd4351a3f6a92471943d8b68925cba1cf5d
oite.diresa.cajamarca@gmail.com <oite.diresa.cajamarca@gmail.com>
2022-11-12 13:11:22 -0500

    helpers user

--- src/comunes/controladores/helpers/helpers.controller.ts
+++ src/comunes/controladores/helpers/helpers.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import axios from 'axios'
 
 @Controller('helpers')
 export class HelpersController {
 
-    @Get('user')
-    async user(token: string) {
+    @Post('user')
+    async user   (@Body()    body: any) {
+        console.log(body.token)
 
-        const resp = await axios.get('http://172.18.20.30:8050/api/user', { headers: { 'Authorization': 'Bearer ' + token } })
+        const resp = await axios.get('http://172.18.20.30:8050/api/user', { headers: { 'Authorization': 'Bearer ' + body.token } })
             .then(data => { console.log(data); return data })
         return resp
 
